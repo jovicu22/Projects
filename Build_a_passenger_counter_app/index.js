72963da5ac939729ff13df1b328f059833370bc4
jovicu22 <jovicuzcode@gmail.com>
2021-07-26 10:33:58 -0600

    update

--- Build_a_passenger_counter_app/index.js
+++ Build_a_passenger_counter_app/index.js
@@ -1,12 +1,19 @@
+
+let saveEl= document.getElementById("save-el")
 let countPeople=document.getElementById("people-counter")
 let count=0
+let ada=0
 
 function increment(){
-    count= count + 1
+    count += 1
     countPeople.innerText=count
 }
 
 function save(){
+    ada= count +"-"
+    saveEl.innerText+= ada
     console.log(count)
+    count = 0
+    countPeople.innerText=count
 }
 
